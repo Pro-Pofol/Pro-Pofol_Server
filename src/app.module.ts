e52e66a63250d970dce3fe8cf374b766395e1eca
soyeonkim0227 <soyeonkim0227@dsm.hs.kr>
2024-04-01 13:46:48 +0900

    REFACT :: env 변수 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ configDotenv();
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
