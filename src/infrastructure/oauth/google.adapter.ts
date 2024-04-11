492ff9d391f0047f380c29a8b8ab8f7e493a01f1
kanghyuk <gi501476@gmail.com>
2024-04-11 14:19:42 +0900

    REFACT :: 얘외처리

--- src/infrastructure/oauth/google.adapter.ts
+++ src/infrastructure/oauth/google.adapter.ts
@@ -4,7 +4,7 @@ import {
   Logger,
   UnauthorizedException,
 } from '@nestjs/common';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { GoogleProfileResponse } from '../../presentation/auth/auth.dto';
 import { ReadGoogleProfilePort } from '../../core/auth/port/auth.out.port';
 
@@ -13,22 +13,27 @@ export class GoogleAuthAdapter implements ReadGoogleProfilePort {
   private readonly logger = new Logger(GoogleAuthAdapter.name);
 
   getGoogleProfile = async (token: string) => {
-    const response = await axios.get<GoogleProfileResponse>(
-      'https://www.googleapis.com/oauth2/v3/userinfo',
-      { params: { alt: 'json', access_token: token } },
-    );
+    const response = await axios
+      .get<GoogleProfileResponse>(
+        'https://www.googleapis.com/oauth2/v3/userinfo',
+        { params: { alt: 'json', access_token: token } },
+      )
+      .catch((e: AxiosError) => {
+        this.logger.error(e.message);
+        this.logger.error(e.response?.data);
 
-    if (response.status != 200) {
-      this.logger.error(response.data);
-
-      if (response.status === 400) {
-        throw new UnauthorizedException('Invalid token');
-      } else if (response.status == 401) {
-        throw new UnauthorizedException('The token has expired');
-      } else {
-        throw new InternalServerErrorException('Some thing went worn in OAuth');
-      }
-    }
+        if (e.response?.status == 401) {
+          throw new UnauthorizedException({
+            status: 401,
+            message: 'The token has expired',
+          });
+        } else {
+          this.logger.error(e.response?.status ?? e.status);
+          throw new InternalServerErrorException(
+            'Some thing went worn in OAuth',
+          );
+        }
+      });
 
     return response.data;
   };
