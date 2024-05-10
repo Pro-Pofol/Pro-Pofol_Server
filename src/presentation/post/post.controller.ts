e1bec201a1cade88354dad855399f0f47f56c169
kanghyuk <gi501476@gmail.com>
2024-05-10 15:06:45 +0900

    자료업로드(파일): Auto stash before merge of "gurdl0525/Post-API-002-자료업로드(파일)" and "main"

--- src/presentation/post/post.controller.ts
+++ src/presentation/post/post.controller.ts
@@ -23,6 +23,7 @@ export class PostController {
   constructor(
     @Inject('postLink')
     private readonly postLinkUseCase: PostLinkUseCase,
+    @Inject('postFile')
     private readonly postFileUseCase: PostFileUseCase,
   ) {}
 
@@ -37,18 +38,18 @@ export class PostController {
     return res.status(201).send();
   }
 
-  @Post('/file/{title}/{type}/{major}')
-  @UseInterceptors(FileInterceptor('file'))
-  async postFile(
-    @UploadedFile() file: Express.Multer.File,
-    @Param('post') title: string,
-    @Param('type') type: PostType,
-    @Param('major') major: Major,
-    @Res() res: Response,
-    @Headers('Authorization') token: string,
-  ): Promise<Response> {
-    await this.postFileUseCase.postFile(title, type, major, token);
-
-    return res.status(201).send();
-  }
+  // @Post('/file/{title}/{type}/{major}')
+  // @UseInterceptors(FileInterceptor('file'))
+  // async postFile(
+  //   @UploadedFile() file: Express.Multer.File,
+  //   @Param('post') title: string,
+  //   @Param('type') type: PostType,
+  //   @Param('major') major: Major,
+  //   @Res() res: Response,
+  //   @Headers('Authorization') token: string,
+  // ): Promise<Response> {
+  //   await this.postFileUseCase.postFile(title, type, major, token);
+  //
+  //   return res.status(201).send();
+  // }
 }
